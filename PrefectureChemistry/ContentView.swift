7cae921e139d44a108841e5b687f491a2377a97f
Yabanin <yabanin@icloud.com>
2024-02-26 11:21:10 +0900

    Add PersonalInfo, YearMonthDay and input field

--- PrefectureChemistry/ContentView.swift
+++ PrefectureChemistry/ContentView.swift
@@ -7,15 +7,50 @@
 
 import SwiftUI
 
+struct YearMonthDay: Codable {
+    let year: Int
+    let month: Int
+    let Day: Int
+}
+
+struct PersonalInfo: Codable {
+    let name: String
+    let birthday: YearMonthDay
+    let blood_type: String
+    let today: YearMonthDay
+    
+}
+
 struct ContentView: View {
+    let bloodTypes = ["a", "b", "ab", "o"]
+    
+    @State private var name = ""
+    @State private var date = Date()
+    @State private var userBloodType = "a"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Text("名前と誕生日、血液型を入力して占う！")
+            TextField("名前", text: $name)
+            DatePicker(
+                "誕生日",
+                selection: $date,
+                displayedComponents: [.date]
+            )
+            .environment(\.locale, Locale(identifier: "ja_JP"))
+            Picker(selection: $userBloodType) {
+                Text("A型").tag("a")
+                Text("B型").tag("b")
+                Text("AB型").tag("ab")
+                Text("0型").tag("o")
+            } label: {
+                Text("血液型")
+            }
+            
+            Button(action: {}, label: {
+                Text("診断する")
+            }).buttonStyle(.borderedProminent)
+        }.padding()
     }
 }
 
