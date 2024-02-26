3677c7e755d7039cb64ba174a10a144150d54ad4
Yabanin <yabanin@icloud.com>
2024-02-26 11:32:21 +0900

    Add Prefecture and MonthDay model

---
+++ PrefectureChemistry/ResultView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultView.swift
+//  PrefectureChemistry
+//
+//  Created by Ryo Hanma on 2024/02/26.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView()
+}
