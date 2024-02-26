e89d05d861493d7b3b7b4b4fe5ecb9948b51d4bc
Yabanin <yabanin@icloud.com>
2024-02-26 10:12:07 +0900

    Initial Commit

---
+++ PrefectureChemistry/PrefectureChemistryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrefectureChemistryApp.swift
+//  PrefectureChemistry
+//
+//  Created by Ryo Hanma on 2024/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PrefectureChemistryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
