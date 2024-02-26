8dd3d22f2e2ad56f65e7bc779c1d611a72050c4c
Yabanin <yabanin@icloud.com>
2024-02-26 11:35:17 +0900

    Add Prefecture and MonthDay model

--- PrefectureChemistry/ResultView.swift
+++ PrefectureChemistry/ResultView.swift
@@ -7,6 +7,22 @@
 
 import SwiftUI
 
+class Prefecture: Codable {
+    let name: String
+    let capital: String
+    let citizen_day: MonthDay?
+    let has_coast_line: Bool
+    let logo_url: String
+    let brief: String
+}
+
+class MonthDay: Codable {
+    let month: Int
+    let day: Int
+}
+
+class PrefectureFetcher: ObservableObject {}
+
 struct ResultView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
